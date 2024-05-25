f11e464d56678b081d966f96c6367dc1793bdbd9
ViolinistaEstudante <132099734+ViolinistaEstudante@users.noreply.github.com>
2024-05-25 13:37:45 -0300

    Site concluído!!

---
+++ script.js
@@ -0,0 +1,11 @@
+// Mostrar o modal
+function showModal() {
+    var modal = document.getElementById("modal");
+    modal.style.display = "block";
+}
+
+// Ocultar o modal
+function hideModal() {
+    var modal = document.getElementById("modal");
+    modal.style.display = "none";
+}
